954ae110f4650a3a0ec6cc8f5bcc99388f9eefcc
Komal Singh <komalcaerusit@gmail.com>
2025-09-19 17:45:14 +0530

    error response handling

--- src/main/java/com/caerus/apigateway/config/SecurityConfig.java
+++ src/main/java/com/caerus/apigateway/config/SecurityConfig.java
@@ -4,14 +4,18 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.ServerAuthenticationEntryPoint;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -41,7 +45,9 @@ public class SecurityConfig {
                         .pathMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**", "/actuator/**").permitAll()
                         .anyExchange().authenticated()
                 )
-                .oauth2ResourceServer(spec -> spec.jwt(Customizer.withDefaults()))
+                .oauth2ResourceServer(spec -> spec
+                        .jwt(Customizer.withDefaults())
+                        .authenticationEntryPoint(customAuthenticationEntryPoint()))
                 .build();
     }
 
@@ -57,5 +63,26 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
         return source;
+    }
+
+    @Bean
+    public ServerAuthenticationEntryPoint customAuthenticationEntryPoint() {
+        return (exchange, ex) -> {
+            var response = exchange.getResponse();
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
+
+            String message = "Missing or invalid token";
+
+            String body = String.format("""
+                    {
+                      "error": "Unauthorized",
+                      "message": "%s"
+                    }
+                    """, message);
+
+            var buffer = response.bufferFactory().wrap(body.getBytes());
+            return response.writeWith(Mono.just(buffer));
+        };
     }
 }
